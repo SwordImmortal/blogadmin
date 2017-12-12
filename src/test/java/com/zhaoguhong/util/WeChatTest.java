72b0de20c620f27320a7c4345c25575249a36804
zhaoguhong <761569559@qq.com>
2017-12-12 11:06:29 +0800

    添加微信发送接口测试

---
+++ src/test/java/com/zhaoguhong/util/WeChatTest.java
@@ -0,0 +1,43 @@
+package com.zhaoguhong.util;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+import com.zhaoguhong.blog.util.HttpUtil;
+import com.zhaoguhong.blog.util.PropertyUtil;
+
+public class WeChatTest {
+  // 一对一
+  private static String sendkey = PropertyUtil.getProperty("sendkey");
+  // 一对多，测试key
+  private static String sendMoney = PropertyUtil.getProperty("sendmoneykey2");
+
+  /**
+   * 微信发送一对一
+   */
+  @Test
+  public void testSend() throws ClientProtocolException, IOException {
+    String url = "https://sc.ftqq.com/";
+    Map<String, Object> params = Maps.newHashMap();
+    params.put("text", "测试一下哈");
+    params.put("desp", "今天天气很好啊");
+    System.out.println(HttpUtil.postMap(url + sendkey + ".send", params));
+  }
+
+  /**
+   * 微信发送一对多
+   */
+  @Test
+  public void testMoney() throws ClientProtocolException, IOException {
+    String url = "https://pushbear.ftqq.com/sub";
+    Map<String, Object> params = Maps.newHashMap();
+    params.put("sendkey", sendMoney);
+    params.put("text", "测试一下哈");
+    params.put("desp", "今天双十二了");
+    System.out.println(HttpUtil.postMap(url, params));
+  }
+}
