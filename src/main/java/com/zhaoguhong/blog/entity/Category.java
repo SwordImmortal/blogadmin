c69948f61cce6e49d869ad04c558f7c68c93e79e
zhaoguhong <761569559@qq.com>
2017-12-12 17:24:27 +0800

    添加类别entity

---
+++ src/main/java/com/zhaoguhong/blog/entity/Category.java
@@ -0,0 +1,40 @@
+package com.zhaoguhong.blog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.zhaoguhong.blog.common.base.BaseEntity;
+
+/**
+ * 分类
+ * 
+ * @author zhaoguhong
+ * @date 2017年12月12日
+ */
+@Entity
+public class Category extends BaseEntity {
+  
+  private Long id;
+
+  /**
+   * 类别名称
+   */
+  private String name;
+
+  @Id
+  @GeneratedValue
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return "Category [id=" + id + ", name=" + name + "]";
+  }
+
+}
