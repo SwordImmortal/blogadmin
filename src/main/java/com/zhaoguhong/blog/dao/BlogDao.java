d2faf673b8022956ddb19707f65aedda2af66896
zhaoguhong <761569559@qq.com>
2017-12-26 23:50:21 +0800

    添加注释

--- src/main/java/com/zhaoguhong/blog/dao/BlogDao.java
+++ src/main/java/com/zhaoguhong/blog/dao/BlogDao.java
@@ -2,7 +2,13 @@ package com.zhaoguhong.blog.dao;
 
 import com.zhaoguhong.blog.common.base.BaseRepository;
 import com.zhaoguhong.blog.entity.Blog;
-
+/**
+ * 
+ * Blog 持久层
+ * 
+ * @author zhaoguhong
+ * @date 2017年12月26日
+ */
 public interface BlogDao extends BaseRepository<Blog> {
 
 }
